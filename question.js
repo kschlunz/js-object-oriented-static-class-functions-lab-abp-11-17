f2165cb23cb516c0f660745f7cd8bfca00bef1d2
Kate Schlunz <schlunzk@gmail.com>
2017-11-17 00:33:56 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,9 +1,11 @@
 'use strict';
 class Question {
  constructor(){
-   this.constructor._All.push(this)
+   this.save()
  }
-
+save(){
+  this.constructor._All.push(this)
+}
   static All(){
       return this._All;
     }
